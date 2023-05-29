385db8676196abd40da0363491c78d2b989e25ad
Larry McCullough <larry.mccullough88@gmail.com>
2023-05-29 03:49:36 -0400

    added text

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>FireGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Share your images with the world. Create memories that will last a life time.</p>
     </div>
   )
 }
